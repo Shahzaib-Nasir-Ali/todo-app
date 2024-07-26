8c66c92d13e5713717003fb891aebd3e8de64435
Moon <moon435664@gmail.com>
2024-07-26 12:14:05 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, { text: todo, completed: false }]);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = todos.slice();
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <AddTodo addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function TodoItem({ todo, onClick }) {
+  return (
+    <li
+      onClick={onClick}
+      style={{
+        textDecoration: todo.completed ? "line-through" : "none",
+        cursor: "pointer",
+      }}
+    >
+      {todo.text}
+    </li>
+  );
+}
+
+export default TodoItem;
